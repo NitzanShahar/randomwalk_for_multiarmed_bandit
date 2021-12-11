4c6d5d06c070065d93d2d70344c71efed7eddf5c
NitzanShahar <68427888+NitzanShahar@users.noreply.github.com>
2021-12-11 12:45:46 +0200

    delete unused sections in the main file and add some results

--- generate_randomwalk.R
+++ generate_randomwalk.R
@@ -25,41 +25,6 @@ library(dplyr)
     #plot
     ggplot(R,aes(x=trial,y=ev,color=arm))+geom_line()
 
+save(R,file='randomwalk_4arms_200trials.rdata')
+write.csv(R,file='randomwalk_4arms_200trials.rdata')
 
-  
-
-
-
-generate_randomwalk<-function(Narms,Ntrials,tau,rho,
-                              upper.bound,lower.bound){
-  browser()
-  #pre-allocate matrix with random starting points
-  R          =matrix(NA,Ntrials,Narms)
-  R[1,]      =lower.bound+(upper.bound-lower.bound)*runif(Narms)
-  
-  
-  #generate the randomwalk
-  for(t in 2:Ntrials){
-    for(arm in 1:Narms){
-    R[t,arm]=R[t-1,]+rho*R[t-1,-arm]+rnorm(1)
-    }
-    R[t,R[t,]>upper.bound]=  upper.bound
-    R[t,R[t,]<lower.bound]=  lower.bound
-  }
-  
-  #add column names
-  colnames(R) <- paste0("arm_", 1:Narms)  
-  
-  return(R)
-  
-}
-
-tau=0.02
-rho=-1
-Narms=4
-tau        = rep(tau,Narms)  
-cor_u      = matrix(rep(rho, Narms^2), nrow = Narms)  
-diag(cor_u)= 1
-Sigma_u    = diag(tau, Narms, Narms) %*%  cor_u %*%  diag(tau, Narms, Narms) #converting cor matrix to var-cov matrix
-
-MASS::mvrnorm(n = 1, rep(0, Narms), Sigma_u)
